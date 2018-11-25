9de146827ec30d1998a34a0394cd8dfc3bca3b0e
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2018-11-25 14:26:15 -0200

    sources and example in a single workspace

--- Example/Pods/Target Support Files/MultiSpecs/MultiSpecs-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MultiSpecs : NSObject
-@end
-@implementation PodsDummy_MultiSpecs
-@end
---
+++ MultiSpecs/MultiSpecs.h
@@ -0,0 +1,19 @@
+//
+//  MultiSpecs.h
+//  MultiSpecs
+//
+//  Created by Felipe Lefèvre Marino on 11/25/18.
+//  Copyright © 2018 Felipe Marino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiSpecs.
+FOUNDATION_EXPORT double MultiSpecsVersionNumber;
+
+//! Project version string for MultiSpecs.
+FOUNDATION_EXPORT const unsigned char MultiSpecsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiSpecs/PublicHeader.h>
+
+
